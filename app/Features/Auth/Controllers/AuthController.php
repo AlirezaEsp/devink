8b5d005a451106d8cc759c3c7842fd9ba6fc76a2
Alireza Esp <alireza.esp07@gmail.com>
2026-08-28 20:48:05 +0330

    add store method to auth controller

--- app/Features/Auth/Controllers/AuthController.php
+++ app/Features/Auth/Controllers/AuthController.php
@@ -3,8 +3,17 @@
 namespace App\Features\Auth\Controllers;
 
 use Illuminate\Http\Request;
+use App\Features\Auth\Requests\RegisterRequest;
+use App\Features\Auth\Services\RegisterService;
+use App\Features\Auth\Resources\UserResource;
 
 class AuthController
 {
-    //
+    public function store(RegisterRequest $request, RegisterService $service): UserResource {
+        $user = $service->execute(
+            $request->validated()
+        );
+
+        return new UserResource($user);
+    }
 }
