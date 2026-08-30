4ef23816d1244cfcdc2e7689e5e0ff44791af7a4
Alireza Esp <alireza.esp07@gmail.com>
2026-08-30 17:37:41 +0330

    add LogoutResponse to Auth feature

---
+++ app/Features/Auth/Resources/LogoutResponse.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Features\Auth\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class LogoutResponse extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'user' => [
+                'id' => $this->resource->id,
+                'email' => $this->resource->email,
+            ],
+            'message' => 'Logged out successfully.'
+        ];
+    }
+}
--- app/Features/Auth/Services/LogoutService.php
+++ app/Features/Auth/Services/LogoutService.php
@@ -6,7 +6,9 @@ use App\Features\Auth\Models\User;
 
 class LogoutService
 {
-    public function logoutUser(User $user): void {
+    public function logoutUser(User $user): User {
         $user->currentAccessToken()->delete();
+
+        return $user;
     }
 }
