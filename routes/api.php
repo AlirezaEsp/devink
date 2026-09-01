cb503acb027df81792a8cf89530b1ecdff715b94
Alireza Esp <alireza.esp07@gmail.com>
2026-09-02 01:32:16 +0330

    add profile routes of Account feature

--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,23 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Features\Auth\Controllers\AuthController;
+use App\Features\Account\Controllers\ProfileController;
 
 Route::prefix('v1')->group(function () {
     // Authentication routes: register, login, logout
-    Route::prefix('auth')->group(function () {
-        Route::post('register', [AuthController::class, 'store'])->name('auth.register');
-        Route::post('login', [AuthController::class, 'login'])->name('auth.login');
-        Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum')->name('auth.logout');
+    Route::prefix('auth')->name('auth.')->group(function () {
+        Route::post('register', [AuthController::class, 'store'])->name('register');
+        Route::post('login', [AuthController::class, 'login'])->name('login');
+        Route::post('logout', [AuthController::class, 'logout'])->name('logout')->middleware('auth:sanctum');
+    });
+
+    // Account routes: profile
+    Route::prefix('account')->name('account.')->group(function () {
+        // Profile routes: store, show, update
+        Route::name('profile.')->middleware('api:sanctume')->group(function () {
+            Route::post('profile', [ProfileController::class, 'store'])->name('store');
+            Route::get('profile', [ProfileController::class, 'show'])->name('show');
+            Route::patch('profile', [ProfileController::class, 'update'])->name('update');
+        });
     });
 });
