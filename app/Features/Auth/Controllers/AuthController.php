1b32079ec82484d2a7d6753a0b0c9e95c8455d54
Alireza Esp <alireza.esp07@gmail.com>
2026-09-15 13:40:41 +0330

    modify AuthController for compatibility

--- app/Features/Auth/Controllers/AuthController.php
+++ app/Features/Auth/Controllers/AuthController.php
@@ -3,14 +3,14 @@
 namespace App\Features\Auth\Controllers;
 
 use App\Features\Auth\Requests\RegisterRequest;
-use App\Features\Auth\Requests\LoginRequest;
-use App\Features\Auth\Requests\LogoutRequest;
 use App\Features\Auth\Services\RegisterService;
+use App\Features\Auth\Responses\RegisterResponse;
+use App\Features\Auth\Requests\LoginRequest;
 use App\Features\Auth\Services\LoginService;
+use App\Features\Auth\Responses\LoginResponse;
+use App\Features\Auth\Requests\LogoutRequest;
 use App\Features\Auth\Services\LogoutService;
-use App\Features\Auth\Resources\RegisterResponse;
-use App\Features\Auth\Resources\LoginResponse;
-use App\Features\Auth\Resources\LogoutResponse;
+use App\Features\Auth\Responses\LogoutResponse;
 
 /**
  * AuthController
@@ -29,7 +29,8 @@ class AuthController
      *
      * @return RegisterResponse
      */
-    public function store(RegisterRequest $request, RegisterService $service): RegisterResponse {
+    public function store(RegisterRequest $request, RegisterService $service): RegisterResponse
+    {
         $user = $service->registerUser(
             $request->validated()
         );
@@ -66,7 +67,8 @@ class AuthController
      *
      * @return LogoutResponse
      */
-    public function logout(LogoutRequest $request, LogoutService $service): LogoutResponse {
+    public function logout(LogoutRequest $request, LogoutService $service): LogoutResponse
+    {
         $user = $service->logoutUser($request->user());
 
         return new LogoutResponse($user);
