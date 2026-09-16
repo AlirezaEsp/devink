666c96aa784d94be7f2c969e46d99baa6c884158
Alireza Esp <alireza.esp07@gmail.com>
2026-09-16 23:39:12 +0330

    redirect root url to api docs

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return response("Hello World!");
-// });
+Route::get('/', function () {
+    return redirect('/docs/api');
+});
