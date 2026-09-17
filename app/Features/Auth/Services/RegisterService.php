dfc27e2fc6032e33b38fc6e68f38f6830d630cc9
Alireza Esp <alireza.esp07@gmail.com>
2026-09-17 16:44:38 +0330

    modify RegisterService to dispatch profile creation related event in a single transaction

--- app/Features/Auth/Services/RegisterService.php
+++ app/Features/Auth/Services/RegisterService.php
@@ -2,8 +2,11 @@
 
 namespace App\Features\Auth\Services;
 
-use App\Features\Auth\Models\User;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+
+use App\Features\Auth\Models\User;
+use App\Features\Auth\Events\UserRegistered;
 
 /**
  * RegisterService
@@ -20,14 +23,28 @@ class RegisterService
      * @return array array containing the user
      */
     public function registerUser(array $data): array {
-        // add user to db
-        $user = User::create([
-            'email' => $data['email'],
-            'password' => Hash::make($data['password'])
-        ]);
-        
-        return [
-            'user' => $user
-        ];
+        return DB::transaction(function () use ($data) {
+            // add user to db
+            $user = User::create([
+                'email' => $data['email'],
+                'password' => Hash::make($data['password'])
+            ]);
+
+            // dispatch event for CreateProfileOnUserRegistered listener
+            UserRegistered::dispatch(
+                $user,
+                [
+                    'username' => $data['username'],
+                    'full_name' => $data['full_name'],
+                    'bio' => $data['bio'] ?? null,
+                    'avatar' => $data['avatar'] ?? null,
+                ]
+            );
+
+            // return user + profile
+            return [
+                'user' => $user->load('profile')
+            ];
+        });
     }
 }
