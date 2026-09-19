65ff32a0194be4c97f5d298dfb26cf93d140302a
Alireza Esp <alireza.esp07@gmail.com>
2026-09-19 17:15:57 +0330

    add UpdateProfile form request

---
+++ app/Features/Account/Requests/UpdateProfileRequest.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateProfileRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'username' => [
+                'required',
+                'string',
+                'max:255',
+                'unique:profiles,username'
+            ],
+            'full_name' => [
+                'required',
+                'string',
+                'max:255'
+            ],
+            'bio' => [
+                'nullable',
+                'string'
+            ],
+            'avatar' => [
+                'nullable',
+                'string'
+            ]
+        ];
+    }
+}
