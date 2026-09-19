2f1a41be6f5fb452ba3272675098e26773ac9d90
Alireza Esp <alireza.esp07@gmail.com>
2026-09-19 17:09:22 +0330

    add show method in ProfileController

--- app/Features/Account/Controllers/ProfileController.php
+++ app/Features/Account/Controllers/ProfileController.php
@@ -3,8 +3,22 @@
 namespace App\Features\Account\Controllers;
 
 use Illuminate\Http\Request;
+use App\Features\Account\Resources\ProfileResource;
 
 class ProfileController
-{
-    //
+{    
+    /**
+     * Show
+     *
+     * @param Request $request Request coming from client
+     *
+     * @return void
+     */
+    public function show(Request $request): ProfileResource
+    {
+        // find user's profile
+        $profile = $request->user()->profile;
+
+        return new ProfileResource($profile);
+    }
 }
