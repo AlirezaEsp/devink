65adb549fa614c5b36e751497c5f084efa9a948a
Alireza Esp <alireza.esp07@gmail.com>
2026-09-19 19:18:38 +0330

    add PublicProfileResource

---
+++ app/Features/Account/Resources/PublicProfileResource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Features\Account\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use App\Features\Account\Models\Profile;
+
+/**
+ * ProfileResource
+ * 
+ * Profile model seriailization
+ * 
+ * @mixin Profile
+ */
+class PublicProfileResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'username' => $this->username,
+            'full_name' => $this->full_name,
+            'bio' => $this->bio,
+            'avatar' => $this->avatar,
+        ];
+    }
+}
