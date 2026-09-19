d1c5923ad822a1bd40afff96b043742a9f9c4ca0
Alireza Esp <alireza.esp07@gmail.com>
2026-09-19 17:50:34 +0330

    add UpdateProfileResponse

---
+++ app/Features/Account/Responses/UpdateProfileResponse.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Features\Account\Responses;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use App\Features\Account\Resources\ProfileResource;
+
+class UpdateProfileResponse extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'message' => 'Profile updated successfully.',
+            'profile' => new ProfileResource($this->resource),
+        ];
+    }
+}
