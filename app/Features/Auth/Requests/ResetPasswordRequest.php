a2fc6a55f0ca8fa8c0c4bff5e47cddc56fc7530d
Alireza Esp <alireza.esp07@gmail.com>
2026-09-20 17:40:25 +0330

    add ForgetPasswordRequest and ResetPasswordRequest

---
+++ app/Features/Auth/Requests/ResetPasswordRequest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Features\Auth\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class ResetPasswordRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function prepareForValidation(): void
+    {
+        $this->merge([
+            'email' => strtolower(trim($this->input('email')))
+        ]);
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'token' => [
+                'required',
+                'string'
+            ],
+            'email' => [
+                'required',
+                'string',
+                'email',
+                'max:255',
+            ],
+            'password' => [
+                'required',
+                'string',
+                'min:8',
+                'confirmed',
+            ],
+        ];
+    }
+}
