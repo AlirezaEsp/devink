f46c73a0d404e6209f20778203ffa2c754ddba71
Alireza Esp <alireza.esp07@gmail.com>
2026-09-20 17:24:22 +0330

    add forget and reset password routes to auth group

--- routes/api.php
+++ routes/api.php
@@ -12,6 +12,8 @@ Route::prefix('v1')->group(function () {
         // without auth
         Route::post('register', [AuthController::class, 'register'])->name('register');
         Route::post('login', [AuthController::class, 'login'])->name('login');
+        Route::post('forgot-password', [AuthController::class, 'forgotPassword'])->name('forgot-password');
+        Route::post('reset-password', [AuthController::class, 'resetPassword'])->name('reset-password');
 
         // with auth
         Route::middleware('auth:sanctum')->group(function () {
